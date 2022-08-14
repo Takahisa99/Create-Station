05f738052198356b859cdf5adadd9fb843fb259a
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-08-14 09:03:35 +0000

    ジャンル、タグ検索機能追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,20 +4,20 @@ class Genre < ApplicationRecord
     validates :name, presence:true, length:{maximum:20}
 
 
-# # 検索方法分岐
-#   def self.looks(search, word)
-#     if search == "perfect_match"
-#       @user = User.where("na LIKE?", "#{word}")
-#     elsif search == "forward_match"
-#       @user = User.where("name LIKE?","#{word}%")
-#     elsif search == "backward_match"
-#       @user = User.where("name LIKE?","%#{word}")
-#     elsif search == "partial_match"
-#       @user = User.where("name LIKE?","%#{word}%")
-#     else
-#       @user = User.all
-#     end
-#   end
+# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("genre LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("genre LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("genre LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("genre LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
 
 end
