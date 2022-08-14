0aeb55ce28a6a7eec359d88d51f41881c8ad2b75
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-08-14 12:07:34 +0000

    機能一通り実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,9 +8,13 @@ class Public::SearchesController < ApplicationController
       if @range == "User"
         @users = User.looks(params[:search], params[:word])
         render :result
-      else
+      elsif @rang == "Create"
         @creates = Create.looks(params[:search], params[:word])
         render :result
+      elsif @range == "Genre"
+        @creates = Create.where(genre_id: params[:genre_id])
+        render :result
+
       end
 
     end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,19 +5,19 @@ class Genre < ApplicationRecord
 
 
 # 検索方法分岐
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("genre LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("genre LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("genre LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("genre LIKE?","%#{word}%")
-    else
-      @user = User.all
-    end
-  end
+#   def self.looks(search, word)
+#     if search == "perfect_match"
+#       @user = User.where("genre LIKE?", "#{word}")
+#     elsif search == "forward_match"
+#       @user = User.where("genre LIKE?","#{word}%")
+#     elsif search == "backward_match"
+#       @user = User.where("genre LIKE?","%#{word}")
+#     elsif search == "partial_match"
+#       @user = User.where("genre LIKE?","%#{word}%")
+#     else
+#       @user = User.all
+#     end
+#   end
 
 
 end
