8d3f2230cc048d64de3077b9a2917ce334a9fa8f
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-08-18 11:32:05 +0000

    usrsのカラム編集

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 2022_08_14_041654) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
-    t.string "introduction"
+    t.string "introduction", default: "", null: false
     t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
